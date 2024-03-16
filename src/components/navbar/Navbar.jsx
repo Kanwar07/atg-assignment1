64df4718536fda1c0e7e87a1671052ba8fe0ead0
Kanwar07 <anmolkanwar07@gmail.com>
2024-03-17 01:11:10 +0530

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import TurnedInIcon from "@mui/icons-material/TurnedIn";
 import NotificationsIcon from "@mui/icons-material/Notifications";
@@ -6,8 +6,21 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import ExploreIcon from "@mui/icons-material/Explore";
 import StarsIcon from "@mui/icons-material/Stars";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import navbarstyle from "./Navbar.module.css";
+import { Menu, MenuItem, Button, Fade } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { grey } from "@mui/material/colors";
 
 function Navbar() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <nav class="flex justify-between pt-2.5 pb-2.5 shadow-2xl pl-[10%] pr-[10%]">
@@ -26,26 +39,26 @@ function Navbar() {
               </div>
             </div>
           </div>
-          <div class="flex flex-row mr-8">
+          <div className={navbarstyle.search}>
             <input
               type="search"
               id="search"
               name="search"
               size="35"
-              class="bg-[#eeeeee] rounded-l-lg pt-1 pb-1 pr-2 pl-2 text-sm"
+              className={navbarstyle.searchtext}
               placeholder="Search here.."
             />
-            <div class="pl-2 pr-2 pt-1 pb-1 bg-[#9a6cb8] rounded-r-lg cursor-pointer">
+            <div className={navbarstyle.searchicon}>
               <SearchIcon color="action" fontSize="small" />
             </div>
           </div>
         </div>
-        <div class="flex flex-row items-center">
+        <div className={navbarstyle.menu}>
           <div class="flex flex-row mr-8 text-[#000000]">
             <div class="mr-2 text-[#9a6cb8]">
               <ExploreIcon />
             </div>
-            <div class="cursor-pointer mr-2">Explore</div>
+            <div class="mr-2">Explore</div>
             <div class="dropdown-center">
               <button
                 type="button"
@@ -57,13 +70,19 @@ function Navbar() {
               </button>
               <ul class="dropdown-menu">
                 <li>
-                  <button class="dropdown-item">Action</button>
+                  <button class="dropdown-item">People - Community</button>
                 </li>
                 <li>
-                  <button class="dropdown-item">Action</button>
+                  <button class="dropdown-item">Places - Venues</button>
                 </li>
                 <li>
-                  <button class="dropdown-item">Action</button>
+                  <button class="dropdown-item">Programs - Events</button>
+                </li>
+                <li>
+                  <button class="dropdown-item">Products - Store</button>
+                </li>
+                <li>
+                  <button class="dropdown-item">Blogs</button>
                 </li>
               </ul>
             </div>
@@ -72,7 +91,7 @@ function Navbar() {
             <div class="mr-2 text-[#9a6cb8]">
               <StarsIcon />
             </div>
-            <div class="cursor-pointer mr-2">Hobbies</div>
+            <div class="mr-2">Hobbies</div>
             <div class="dropdown-center">
               <button
                 type="button"
@@ -84,24 +103,30 @@ function Navbar() {
               </button>
               <ul class="dropdown-menu">
                 <li>
-                  <button class="dropdown-item">Action</button>
+                  <button class="dropdown-item">People - Community</button>
                 </li>
                 <li>
-                  <button class="dropdown-item">Action</button>
+                  <button class="dropdown-item">Places - Venues</button>
                 </li>
                 <li>
-                  <button class="dropdown-item">Action</button>
+                  <button class="dropdown-item">Programs - Events</button>
+                </li>
+                <li>
+                  <button class="dropdown-item">Products - Store</button>
+                </li>
+                <li>
+                  <button class="dropdown-item">Blogs</button>
                 </li>
               </ul>
             </div>
           </div>
-          <div class="mr-6 text-[#9a6cb8]">
+          <div class="mr-6 text-[#9a6cb8] cursor-pointer">
             <TurnedInIcon />
           </div>
-          <div class="mr-6 text-[#9a6cb8]">
+          <div class="mr-6 text-[#9a6cb8] cursor-pointer">
             <NotificationsIcon />
           </div>
-          <div class="mr-6 text-[#9a6cb8]">
+          <div class="mr-6 text-[#9a6cb8] cursor-pointer">
             <ShoppingCartIcon />
           </div>
           <div>
@@ -109,6 +134,47 @@ function Navbar() {
               Sign In
             </button>
           </div>
+        </div>
+        <div className={navbarstyle.menudata}>
+          <div className={navbarstyle.searchiconinside}>
+            <SearchIcon color="action" fontSize="small" />
+          </div>
+          <div class="mr-6 text-[#9a6cb8]">
+            <NotificationsIcon />
+          </div>
+          <Button
+            id="fade-button"
+            aria-controls={open ? "fade-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+            className={navbarstyle.button}
+          >
+            <MenuIcon fontSize="small" sx={{ color: grey[800] }} />
+          </Button>
+          <Menu
+            id="fade-menu"
+            MenuListProps={{
+              "aria-labelledby": "fade-button",
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            TransitionComponent={Fade}
+          >
+            <MenuItem class="flex flex-row text-[#9a6cb8] pl-2 pr-2 mt-2 mr-1 ml-1">
+              <div class="mr-2 text-[#9a6cb8]">
+                <ExploreIcon />
+              </div>
+              Explore
+            </MenuItem>
+            <MenuItem class="flex flex-row text-[#9a6cb8] pl-2 pr-2 mt-2 mb-2 mr-1 ml-1">
+              <div class="mr-2 text-[#9a6cb8]">
+                <StarsIcon />
+              </div>
+              Hobbies
+            </MenuItem>
+          </Menu>
         </div>
       </nav>
     </>
